da918e2f8912f9d779258a6562ef5e22f59b79bf
kirkwood616 <kirkwood616@gmail.com>
2022-01-11 15:16:40 -0500

    Added styling & building votes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import AdDesigner from "./components/AdDesigner";
+import Votes from "./components/Votes";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<AdDesigner />
+			<div className="BodyComponents">
+				<AdDesigner />
+				<Votes />
+			</div>
 		</div>
 	);
 }
--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,1 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Votes.css";
+
+function Votes() {
+	const [chocolateVotes, setChocolateVotes] = useState(0);
+	const [vanillaVotes, setVanillaVotes] = useState(0);
+	const [strawberryVotes, setStrawberryVotes] = useState(0);
+	let totalVotes = chocolateVotes + vanillaVotes + strawberryVotes;
+	let chocolatePercent = (chocolateVotes / totalVotes) * 100;
+	let vanillaPercent = (vanillaVotes / totalVotes) * 100;
+	let strawberryPercent = (strawberryVotes / totalVotes) * 100;
+
+	return (
+		<div id="Votes">
+			<h2>Vote Here</h2>
+			<button>Chocolate</button>
+			<button>Vanilla</button>
+			<button>Strawberry</button>
+			<p>
+				Chocolate: {chocolateVotes}({chocolatePercent}%)
+			</p>
+			<div className="chocolateVotes"></div>
+			<p>
+				Vanilla: {vanillaVotes}({vanillaPercent}%)
+			</p>
+			<div className="vanillaVotes"></div>
+			<p>
+				Strawberry: {strawberryVotes}({strawberryPercent}%)
+			</p>
+			<div className="strawberryVotes"></div>
+		</div>
+	);
+}
+
+export default Votes;
