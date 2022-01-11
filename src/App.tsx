a5e4f1817845864b1b1fa719ede0a2c4f972ffa3
kirkwood616 <kirkwood616@gmail.com>
2022-01-11 14:04:50 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import AdDesigner from "./components/AdDesigner";
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<AdDesigner />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/AdDesigner.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./AdDesigner.css";
+
+function AdDesigner() {
+	const [flavor, setFlavor] = useState("");
+	const [colorTheme, setColorTheme] = useState(true);
+	let addTheme = "";
+	addTheme = colorTheme ? " lightTheme" : " darkTheme";
+	const [fontSize, setFontSize] = useState(40);
+	return (
+		<div id="AdDesigner">
+			<h2>Ad Designer</h2>
+			<div className={"adContainer" + addTheme}>
+				<p>Vote For</p>
+				<h3 style={{ fontSize: `${fontSize}px` }}>{flavor}</h3>
+			</div>
+			<p>What to Support</p>
+			<button onClick={() => setFlavor("Chocolate")}>Chocolate</button>
+			<button onClick={() => setFlavor("Vanilla")}>Vanilla</button>
+			<button onClick={() => setFlavor("Strawberry")}>Strawberry</button>
+			<p>Color Theme</p>
+			<button onClick={() => setColorTheme(true)}>Light</button>
+			<button onClick={() => setColorTheme(false)}>Dark</button>
+			<p>Font Size</p>
+			<button onClick={() => setFontSize(fontSize - 1)}>Down</button>
+			<span className="fontSize">{fontSize}</span>
+			<button onClick={() => setFontSize(fontSize + 1)}>Up</button>
+		</div>
+	);
+}
+
+export default AdDesigner;
