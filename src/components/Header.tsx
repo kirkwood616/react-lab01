586fea2d4613f99590d464c43c207b215606d888
kirkwood616 <kirkwood616@gmail.com>
2022-01-12 12:02:58 -0500

    Cleaned up code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,7 @@ function Header({ user }: Prop) {
 	return (
 		<header>
 			<h1>Ice Cream Wars</h1>
-			{/* <div> */}
 			<span className="fontLight">Welcome {user}</span>
-			{/* </div> */}
 		</header>
 	);
 }
